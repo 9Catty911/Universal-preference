5acf0cacb1c8847e613497fc3bacd7d391dc5c9d
Programming <programming@macbook-pro-programming-6.local>
2019-04-13 16:28:39 +0300

    13.04.19

---
+++ Universal preference/View/ImageCell.swift
@@ -0,0 +1,11 @@
+//
+//  ImageCell.swift
+//  Universal preference
+//
+//  Created by Programming on 13.04.2019.
+//  Copyright © 2019 gaudi. All rights
+import UIKit
+
+class ImageCell: UITableViewCell {
+    @IBOutlet weak var customImageView: UIImageView!
+}
